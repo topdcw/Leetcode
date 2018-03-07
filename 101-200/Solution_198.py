c6257a5c5f0206c57a91ccba38aef1e579d24322
Guenon <topdcw@users.noreply.github.com>
2018-03-07 08:13:40 +0800

    Solution198
    使用动态规划
    data（n）是data（n-1）与data（n-2）+nums(n)中的较大者

---
+++ 101-200/Solution_198.py
@@ -0,0 +1,44 @@
+#使用动态规划
+
+class Solution:
+    def rob(self, nums):
+        lens=len(nums)
+        data=[]
+        if lens>0:
+            data.append(nums[0])
+            if lens==1:
+                return data[0]
+            else:
+                if nums[1]>nums[0]:
+                    data.append(nums[1])
+                else:
+                    data.append(nums[0])
+        else:
+            return 0
+        #从第三位开始计算
+        count=3
+        while count<=lens:
+            tmp1=nums[count-1]+data[count-3]
+            tmp2=data[count-2]
+            if tmp1>tmp2:
+                data.append(tmp1)
+            else:
+                data.append(tmp2)
+            count+=1
+        #寻找data中的最大值
+        return max(data)
+
+
+
+
+    def myrob(self,nums,lens):
+        if lens==1 :
+            return nums[0]
+        elif lens==2 :
+            return nums[0] if nums[0]>nums[1] else nums[1]
+        else :
+            return myrob(nums,lens-1)
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
